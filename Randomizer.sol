ac6e9dbec8212996f54a63f550104542736681fb
Marco Graziano <marco@visiblenergy.com>
2018-07-25 06:31:13 -0700

    added natspec style comments

--- Randomizer.sol
+++ Randomizer.sol
@@ -1,29 +1,21 @@
 /*
- * Sample application for testing.
- *
  * Copyright (C) 2018, Visible Energy Inc. and the Blockduino contributors.
- *
  */
 pragma solidity ^0.4.24;
 
-// to use with remixd with mount point at the same level of Blockduino repos
 import "https://github.com/Blockduino/Contracts/BlockduinoSDK.sol";
 
-/*
- * Example of using a Blockduino board used to obtain a true random number generated
- * by a hardware device connected to its serial port.
- *
- * Send RNG_FEE ether to the generate() function to have the hardware random number
- * generator controlled by the Blockduino board returning a true random number.
- *
- * All the details and communication with the Blockduino board is handled by the
- * usingBlockduinoSDK base contract.
- */
+// to use with remixd with mount point at the same level of Blockduino repos:
+// import localhost/contracts/BlockduinoSDK.sol;
+
+/// @title A random number generator using a TrueRNG device controlled by a Blockduino board 
+/// @notice This is a contract showing the use of the BlockduinoSDK
+/// @dev Communication with the Blockduino board is handled by the usingBlockduinoSDK base contract 
 contract Randomizer is usingBlockduinoSDK {
 	address public owner;   // contract owner
     address public device;  // the device address of the Blockduino with the RNG
     
-	// event to watch in any dApp using this contract 
+	/// @dev event to watch in any dApp using this contract 
 	event RandomNumber(string log, address _device, uint64 error, string response);
 	
     string public randomNumber;         // last random number generated
@@ -32,10 +24,10 @@ contract Randomizer is usingBlockduinoSDK {
     uint public RNG_FEE = 0.02 ether;   // payment for operating the hardware RNG
     uint256 public req_value;           // actual amount paid to the device account
 
-    /* 
-     * Deploy this contract passing the address of the Blockduino core contract and the
-     * address of Blockduino device controlling the hardware RNG.
-     */
+    
+    /// @dev Deploy this contract passing the address of the deployed Blockduino core contract 
+    /// and the address of the Blockduino device controlling the TrueRNG hardware.
+    /// @dev Deposit some funds in the contract to pay gas for the transactions.
 	constructor (address BDcontract, address BDboard) usingBlockduinoSDK(BDcontract, msg.sender) public payable {
         // the constructor is payable so that funds can be sent to it at Deploy time
         // funds are needed in the contract to pay for gas of transfer functions 
@@ -46,7 +38,7 @@ contract Randomizer is usingBlockduinoSDK {
     // fallback function
     function() public payable {} // fallback must be payable for the contract to take a payment
 
-
+    // Convert a bytes32 to a string 
     function bytes32ToString(bytes32 x) pure private returns (string) {
         bytes memory bytesString = new bytes(32);
         uint charCount = 0;
@@ -64,10 +56,9 @@ contract Randomizer is usingBlockduinoSDK {
         return string(bytesStringTrimmed);
     }
 
-    /*
-     * The function called by the Blockduino board in the response transaction with
-     * the result of the RPC request received from generate().
-     */
+    /// @dev The function called by the Blockduino board as response transaction to the RPC sent to the board
+    /// @param respData the result of the Blockduino RPC
+    /// @param error any error from the Blockduino
    	function readRandomCallback(uint64 error, bytes32 respData) public {
     	// safety check: sender must be the Blockduino contract (address inherited from SDK)
     	if (msg.sender != address(CONTRACT)) {
@@ -81,13 +72,12 @@ contract Randomizer is usingBlockduinoSDK {
     	// for any dApp monitoring the device to get the randomNumber
     	emit RandomNumber("received from Randomizer", device, error, randomNumber);
     }
-    // readRandomCallback function signature used as function ID
+    /// @dev readRandomCallback function signature used as function ID
     bytes4 constant RNG_CB_FID = bytes4(keccak256("readRandomCallback(uint64,bytes32)"));
     
-    /*
-     * Generate a true random number by reading the serial port of the Blockduino board
-     * where the hardware RNG device is connected.
-     */
+     /// @notice Send `RNG_FEE` ether to the Blockduino account `device` to generate a true random number from the device.
+     /// @dev The random number is obtained by reading the serial port of the Blockduino board
+     /// where the TureRNG device is connected to.
     function generate() public payable {
         int req_reply;
 
